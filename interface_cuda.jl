98854cbee92d4daf81b76761b6a5dfa4e43abcd2
Gautam Luhana <gkluhana@gmail.com>
2024-02-14 12:14:18 -0800

    julia codes

--- interface_cuda.jl
+++ interface_cuda.jl
@@ -9,6 +9,14 @@ const libSlater = "/arc/project/st-greif-1/gkluhana/cuslater/libcuSlater.so"  # 
 # define the structs and function defined in C
 struct InputData
     c::Vector{Float64}
+# Load the shared library
+const libSlater = "/arc/project/st-greif-1/gkluhana/cuslater/libcuSlater.so"  # Update the path
+# define the structs and function defined in C
+struct InputData
+    c1::Vector{Float64}
+    c2::Vector{Float64}
+    c3::Vector{Float64}
+    c4::Vector{Float64}
     r::Float64
     w::Vector{Float64}
     xrange::Vector{Float64}
@@ -153,7 +161,7 @@ function evaluate_inner_stream(input_data::InputDataStreams, gpu_num::Int )
 				gpu_num )
 end
 function evaluate_inner(input_data::InputData, gpu_num::Int, result::Ptr{Float64})
-	sum_integral = ccall((cuslater::evaluateInner, libSlater), Cdouble, 
+	sum_integral = ccall((:evaluateInner, libSlater), Cdouble, 
 				(Ptr{Cdouble},Ptr{Cdouble},Ptr{Cdouble},Ptr{Cdouble}, 
 				Cdouble, 
 				Ptr{Cdouble}, 
@@ -161,6 +169,7 @@ function evaluate_inner(input_data::InputData, gpu_num::Int, result::Ptr{Float64
 				Cint,Cint,Cint, 
 				Ptr{Cdouble},
 				Cint), 
+				Ptr{Cdouble}), 
 				input_data.c1,input_data.c2,input_data.c3,input_data.c4, 
 				input_data.r, 
 				input_data.w, 
@@ -215,5 +224,7 @@ function test_evaluate_streams()
 		  )
 	sum = evaluate_inner_stream(input_data, 0)
 	println("sum = ",sum)
+
 end
+
 
