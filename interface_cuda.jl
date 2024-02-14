376048122387fedd88d5dd326a846218e8a6bb26
Gautam Luhana <gkluhana@gmail.com>
2024-02-14 12:14:18 -0800

    julia codes

---
+++ interface_cuda.jl
@@ -0,0 +1,33 @@
+# Load the shared library
+const libSlater = "/arc/project/st-greif-1/gkluhana/cuslater/libcuSlater.so"  # Update the path
+# define the structs and function defined in C
+struct InputData
+    c1::Vector{Float64}
+    c2::Vector{Float64}
+    c3::Vector{Float64}
+    c4::Vector{Float64}
+    r::Float64
+    w::Vector{Float64}
+    xrange::Vector{Float64}
+    yrange::Vector{Float64}
+    zrange::Vector{Float64}
+    x_axis_points::Int64
+    z_axis_points::Int64
+    y_axis_points::Int64
+end
+function evaluate_inner(input_data::InputData, result::Ptr{Cdouble})
+	sum_integral = ccall((:evaluateInner, libSlater), Cdouble, 
+				(Ptr{Cdouble},Ptr{Cdouble},Ptr{Cdouble},Ptr{Cdouble}, 
+				Cdouble, 
+				Ptr{Cdouble}, 
+				Ptr{Cdouble},Ptr{Cdouble},Ptr{Cdouble},
+				Cint,Cint,Cint, 
+				Ptr{Cdouble}), 
+				input_data.c1,input_data.c2,input_data.c3,input_data.c4, 
+				input_data.r, 
+				input_data.w, 
+				input_data.xrange,input_data.yrange,input_data.zrange,  
+				input_data.x_axis_points,input_data.y_axis_points,input_data.z_axis_points, result)
+end
+
+
---
+++ test.jl
@@ -0,0 +1,28 @@
+# Set up the interface with the cuda code
+include("interface_cuda.jl")
+
+#initialize data
+data = InputData(
+    [0.0,0.0,0.0],	#c1
+    [1.0,0.0,0.0],	#c2
+    [2.0,0.0,0.0],	#c3
+    [3.0,0.0,0.0],	#c4
+    1.0,               # r
+    [0.1,0.2,0.3],	#w
+    [-5.0,10.0],	#xrange
+    [-5.0,10.0],	#yrange
+    [-5.0,10.0],	#zrange
+    500,              # x_axis_points
+    500,              # y_axis_points
+    500               # z_axis_points
+)
+
+# Vector for storing integrand evaluation
+f_evals = zeros(Float64,data.x_axis_points*data.y_axis_points*data.z_axis_points)
+f_evals_ptr = pointer(f_evals)
+
+# Call the julia wrapper for the C function
+integral_sum = evaluate_inner(data,f_evals_ptr)
+
+# Print the result of evaluate_inner
+println("Result of evaluate_inner: ", integral_sum)
