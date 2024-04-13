52b9807466e1cf3e0b648487d0ae4f4a821d273f
Avleen Kaur <git.avleenk@gmail.com>
2024-04-13 01:51:01 -0700

    type flexible grids

--- grids.jl
+++ grids.jl
@@ -1,9 +1,13 @@
 
-using FastGaussQuadrature, Plots, LazyGrids, Lebedev
+using FastGaussQuadrature, Plots, Lebedev
 using DelimitedFiles
 
-function making_zeros(x)
-    ϵ=eps()
+println("Current files are being stored in:")
+println(pwd())
+
+#function for zeroing out the numbers with magnitude less than machine precision
+function making_zeros(x,precision_type=Float64)
+    ϵ=eps(precision_type)
         for i in eachindex(x)
             if abs(x[i]) < ϵ
             x[i] = 0
@@ -11,55 +15,87 @@ function making_zeros(x)
         end
     return x
 end
-function comp_sim(a,b,n)
-    h=(b-a)/n
-    nodes=a:h:b
-    weights = h*(3/8)*ones(n+1)
-    for i in eachindex(weights)
-        if ((mod(i-1,3)==0 && i >1) && (i < (n+1)))
-            weights[i]=weights[i]*2
-        elseif ((i > 1) && (i < (n+1)))
-            weights[i]=weights[i]*3
-        end
+
+#code for composite Simpson 3/8th
+function comp_sim_new(a,b,n)
+	#n must be multiple of 3
+	if n%3 != 0
+        n=3*ceil(n/3)
     end
-    return nodes,weights,h
+	h=(b-a)/n
+	#equidistant nodes
+	nodes=a:h:b
+	#weights for simpson 3/8th
+	weights = ones(n+1) 
+	weights[2:3:n] .= 3
+	weights[3:3:n] .= 3
+	weights[4:3:n] .= 2
+	weights = weights * h * (3/8)
+	return nodes,weights,h
 end
 
-function generate_grid_r_laguerre(nr_start::Int=10)
+#Gauss Generalized Laguerre grid for a given precision_type = Float64, Float32, so on...
+function generate_grid_r_laguerre(nr_start::Int=10,precision_type=Float64)
 	# r points
-	rnodes, rweights = gausslaguerre(nr_start,1)
-	rweights=making_zeros(rweights)
+	#alpha = 1 for generalized Laguerre
+	rnodes, rweights = gausslaguerre(nr_start,1) 
+	rnodes=precision_type.(rnodes)
+	rweights=precision_type.(rweights)
+	rweights=making_zeros(rweights,precision_type)
 	rweights_new=rweights[rweights .!= 0]
 	rnodes_new=rnodes[rweights .!= 0]
 	nr=length(rnodes_new)
-	r_file = "grid_files/r_$(nr).grid"
+	if precision_type == Float64
+		r_file = "grid_files_adap/lag64/r_$(nr_start)_$(nr).grid"
+	end
+	if precision_type == Float32
+		r_file = "grid_files_adap/lag32/r_$(nr_start)_$(nr).grid"
+	end
     writedlm(r_file, [rnodes_new rweights_new])
 	return nr
 end
 
-function generate_grid_l_lebedev(nl_start::Int=2)
+#Gauss Legendre grid for a given precision_type = Float64, Float32, so on...
+function generate_grid_l_lebedev(nl_start::Int=2,precision_type=Float64)
 	#lebedev points
     avail=getavailablepoints()
 	nl= avail[nl_start]
 	lnodes_x, lnodes_y, lnodes_z, lweights =  lebedev_by_points(nl)
-	lweights=making_zeros(lweights)
-	lweights_new=lweights[lweights .!= 0]
+	lnodes_x = precision_type.(lnodes_x)
+	lnodes_y = precision_type.(lnodes_y)
+	lnodes_z = precision_type.(lnodes_z)
+	lweights = precision_type.(lweights)	
+	lweights = making_zeros(lweights,precision_type)
+	lweights_new = lweights[lweights .!= 0]
 	lxgrid = lnodes_x[lweights .!= 0]
 	lygrid = lnodes_y[lweights .!= 0]
 	lzgrid = lnodes_z[lweights .!= 0]
 	nl = length(lzgrid)
-	l_file = "grid_files/l_$(nl).grid"
+	if precision_type == Float64
+		l_file = "grid_files_adap/leb64/l_$(nl).grid"
+	end
+	if precision_type == Float32
+		l_file = "grid_files_adap/leb32/l_$(nl).grid"
+	end
     writedlm(l_file, [lxgrid lygrid lzgrid lweights_new],' ')
 	return nl
 end
 
-function generate_grid_x1_simpson(a,b,n)
+#generating grid file for Simpson for a given precision_type = Float64, Float32, so on...
+function generate_grid_x1_simpson(a,b,n,precision_type=Float64)
 	nodes, weights = comp_sim(a,b,n)
-	weights=making_zeros(weights)
+	nodes = precision_type.(nodes)
+	weights = precision_type.(weights)
+	weights=making_zeros(weights,precision_type)
 	weights_new=weights[weights .!= 0]
 	nodes_new=nodes[weights .!= 0]
 	nx = length(weights_new)
-	x1_file = "grid_files/x1_simpson_1d_$(nx-1).grid"
+	if precision_type == Float64
+		x1_file = "grid_files_adap/sim64/x1_simpson_1d_$(nx-1).grid"
+	end
+	if precision_type == Float32
+		x1_file = "grid_files_adap/sim32/x1_simpson_1d_$(nx-1).grid"
+	end
 	open(x1_file, "w") do file
 			# Write a and b values in the first two lines
 			println(file, a, " ", b)
@@ -70,21 +106,24 @@ function generate_grid_x1_simpson(a,b,n)
 	return nx
 end
 
-function generate_grid_x1_legendre(a,b,n)
-		nodes, weights = gausslegendre(n)
-        nodes = ((b-a)/2)*nodes .+ ((a+b)/2)
-        weights = ((b-a)/2)*weights
-        weights=making_zeros(weights)
-        weights_new=weights[weights .!= 0]
-        nodes_new=nodes[weights .!= 0]
-		nx = length(weights_new)
-		x1_file = "grid_files/x1_legendre_1d_$(nx).grid"
-		open(x1_file, "w") do file
-			# Write a and b values in the first two lines
-			println(file, a, " ", b)
-			# Write nodes_new and weights_new
-			writedlm(file, [nodes_new weights_new], ' ')
-    	end
-
-		return nx
+#generating grid file for Legendre for a given precision_type = Float64, Float32, so on...
+function generate_grid_x1_legendre(n,precision_type=Float64)
+	nodes, weights = gausslegendre(n)
+	nodes = precision_type.(nodes)
+	weights = precision_type.(weights)
+	weights=making_zeros(weights,precision_type)
+	weights_new=weights[weights .!= 0]
+	nodes_new=nodes[weights .!= 0]
+	nx = length(weights_new)
+	if precision_type == Float64
+		x1_file = "grid_files_adap/leg64/x1_legendre_1d_$(nx).grid"
+	end
+	if precision_type == Float32
+		x1_file = "grid_files_adap/leg32/x1_legendre_1d_$(nx).grid"
+	end
+	open(x1_file, "w") do file
+		# Write nodes_new and weights_new
+		writedlm(file, [nodes_new weights_new], ' ')
+	end
+	return nx
 end
--- readme.md
+++ readme.md
@@ -1,6 +1,17 @@
 Documentation for the code
 
-How to run it on Sockeye
+## Grids
+
+There are two Julia files for grids. 
+	1. `grids.jl` evaluates grids.
+			 I have updated grids.jl file to be type flexible. I am storing the grids in `grids_files_adap`
+	2. `print_grids.jl` prints grids and saves them in `grids_files_adap` folder.
+			Please edit this file and the path in `grids.jl` for calculating new grids.
+
+
+## Sockeye
+
+Instructions for running on Sockeye.
 
 Once you're in cuslater directory, run the following commands:
 
@@ -10,5 +21,5 @@ Once you're in cuslater directory, run the following commands:
 		```srun --account=st-greif-1-gpu --partition=interactive_gpu --time=3:0:0 -N 1 -n 2 --mem=32G --gpus=1 --cpus-per-task 1 --pty /bin/bash```
 	4. ./simple
 	
-
+## Other Comments
 Other comments by Gautam: all of the relevant code is in `src/evalIntegral.cu`
