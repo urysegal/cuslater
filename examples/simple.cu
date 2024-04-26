a3d4cc6a6556c1e16e44557c625f614ab12be648
Gautam Luhana <gkluhana@gmail.com>
2024-04-26 13:51:11 -0700

    updt

--- examples/simple.cu
+++ examples/simple.cu
@@ -30,11 +30,11 @@ main(int argc, const char *argv[])
 
         const std::string x1_type = "legendre"; //legendre or simpson
 
-	auto start = std::chrono::high_resolution_clock::now();
+        auto start = std::chrono::high_resolution_clock::now();
         auto sum = cuslater::evaluateFourCenterIntegral(c,nr,nl,nx,x1_type,tol);
-//        auto sum = cuslater::evaluateFourCenterIntegral(c,nr,nl,nx,x1_type,4);
-	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        auto sum = cuslater::evaluateFourCenterIntegral(c,nr,nl,nx,x1_type,4);
+        auto end = std::chrono::high_resolution_clock::now();
+	    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         std::cout << "nr: " << nr << " nl: " << nl << " nx: " << nx << std::endl;
         std::cout << "result: " << std::fixed << std::setprecision(std::numeric_limits<double>::max_digits10) << sum << std::endl;
         std::cout << "Time Elapsed: " << duration.count()/1e6 << " seconds" << std::endl;
