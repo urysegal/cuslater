482921de13695faf287242df395a8f70a927a37e
Avleen Kaur <git.avleenk@gmail.com>
2024-05-08 16:04:17 -0700

    fixed again generate x1 from std

--- src/evalIntegral.cu
+++ src/evalIntegral.cu
@@ -108,13 +108,14 @@ namespace cuslater{
 		float factor = (a - b) / 2.0;
 		float node;
 		float weight;
-		for (int i = 0; i < x1_standard_nodes.size(); ++i) {
+		for (std::vector<float>::size_type i = 0; i < x1_standard_nodes.size(); ++i) {
 			node = x1_standard_nodes[i] * factor + shift;
 			x1_nodes.push_back(node);
 			weight = x1_standard_weights[i] * factor;
 			x1_weights.push_back(weight);
 		}
 	}
+
 
 
     double evaluateFourCenterIntegral( float* c,
