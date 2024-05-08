55f3180e8bb693aac28b18b48c43880f0984c719
Avleen Kaur <git.avleenk@gmail.com>
2024-05-08 16:00:20 -0700

    fixed generate x1 from std

--- src/evalIntegral.cu
+++ src/evalIntegral.cu
@@ -103,26 +103,18 @@ namespace cuslater{
 		        			d_sum);
 	            return delta_sum;
 	    }//evaluateInner
-     void generate_x1_from_std(float a, float b, std::vector<float>* x1_standard_nodes, std::vector<float>* x1_standard_weights, std::vector<float>* x1_nodes, std::vector<float>* x1_weights) { 
+     void generate_x1_from_std(float a,float b,std::vector<float>* x1_standard_nodes, std::vector<float>* x1_standard_weights, std::vector<float>* x1_nodes, std::vector<float>* x1_weights){ 
 		float shift  = (a + b) / 2.0; 
 		float factor = (a - b) / 2.0;
 		float node;
 		float weight;
-		
-		// Ensure the vectors are not null pointers and have the same size
-		if (x1_standard_nodes && x1_standard_weights && x1_nodes && x1_weights &&
-			x1_standard_nodes->size() == x1_standard_weights->size()) {
-			for (int i = 0; i < x1_standard_nodes->size(); ++i) {
-				// Access elements using ->at() or (*vector)[index]
-				node = (*x1_standard_nodes)[i] * factor + shift;
-				x1_nodes->push_back(node);
-				weight = (*x1_standard_weights)[i] * factor;
-				x1_weights->push_back(weight);
-			}
-		} else {
-			std::cerr << "Invalid input vectors or mismatched sizes." << std::endl;
-		}
-	}
+		for (int i =0; i < x1_standard_nodes.size() ; ++i) {
+			node = x1_standard_nodes[i] * factor  + shift;
+			x1_nodes->push_back(node);
+			weight = x1_standard_weights[i] * factor;
+			x1_weights->push_back(weight);
+		}	
+	 }
 
     double evaluateFourCenterIntegral( float* c,
                                 int nr,  int nl,  int nx,
