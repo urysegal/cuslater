f513e4b0f5444b11f3c3bd314e9984fbd99564f9
MarkEwert03 <mewert03@gmail.com>
2024-05-14 13:32:26 -0700

    formated files with cpp extention Prettier

--- examples/simple.cu
+++ examples/simple.cu
@@ -2,40 +2,41 @@
 // Created by gkluhana on 26/03/24.
 //
 
-#include "../include/evalIntegral.h"
-#include <iomanip> // for std::setprecision
-#include <iostream>
 #include <chrono>
+#include <iomanip>  // for std::setprecision
+#include <iostream>
+
+#include "../include/evalIntegral.h"
 using namespace std;
 
-int
-main(int argc, const char *argv[])
-{
-        int nr = 89;
-        int nl = 590;
-        int nx = 375;
-        double tol = 1e-10;	
-	if ( argc == 2 and argv[1][0] == 'd' ) {
-		nr = 2;
-		nl = 6;
-		nx = 21;
-	} 
-	if ( argc == 3 and argv[1][0] == 't' ) {
-		tol = std::stod(argv[2]);	
-	} 
-        float c[] = {0,0,0,
-                      1,0,0,
-                      2,0,0,
-                      3,0,0};
+int main(int argc, const char *argv[]) {
+    int nr = 89;
+    int nl = 590;
+    int nx = 375;
+    double tol = 1e-10;
+    if (argc == 2 and argv[1][0] == 'd') {
+        nr = 2;
+        nl = 6;
+        nx = 21;
+    }
+    if (argc == 3 and argv[1][0] == 't') {
+        tol = std::stod(argv[2]);
+    }
+    float c[] = {0, 0, 0, 1, 0, 0, 2, 0, 0, 3, 0, 0};
 
-        const std::string x1_type = "legendre"; //legendre or simpson
+    const std::string x1_type = "legendre";  // legendre or simpson
 
-        auto start = std::chrono::high_resolution_clock::now();
-        auto sum = cuslater::evaluateFourCenterIntegral(c,nr,nl,nx,x1_type,tol);
-        //auto sum = cuslater::evaluateFourCenterIntegral(c,nr,nl,nx,x1_type,4);
-        auto end = std::chrono::high_resolution_clock::now();
-	    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        std::cout << "nr: " << nr << " nl: " << nl << " nx: " << nx << std::endl;
-        std::cout << "result: " << std::fixed << std::setprecision(std::numeric_limits<double>::max_digits10) << sum << std::endl;
-        std::cout << "Time Elapsed: " << duration.count()/1e6 << " seconds" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    auto sum =
+        cuslater::evaluateFourCenterIntegral(c, nr, nl, nx, x1_type, tol);
+    // auto sum = cuslater::evaluateFourCenterIntegral(c,nr,nl,nx,x1_type,4);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "nr: " << nr << " nl: " << nl << " nx: " << nx << std::endl;
+    std::cout << "result: " << std::fixed
+              << std::setprecision(std::numeric_limits<double>::max_digits10)
+              << sum << std::endl;
+    std::cout << "Time Elapsed: " << duration.count() / 1e6 << " seconds"
+              << std::endl;
 }
--- include/evalIntegral.h
+++ include/evalIntegral.h
@@ -1,39 +1,31 @@
 //
 // Created by gkluhana on 26/03/24.
 //
-#include <vector>
-#include "cuslater.cuh"
-#include "utilities.h"
-#include "grids.h"
 #include <thrust/device_vector.h>
 #include <thrust/reduce.h>
-//#include "evalInnerIntegral.h"
-namespace cuslater{
-	__global__ void accumulateSum(double result, 
-						float r_weight, 
-						float l_weight, 
-						double* __restrict__ d_sum);
-	__global__
-	void evaluateIntegrandReduceZ(int x_dim,
-	                                      float r, float l_x, float l_y, float l_z,
-	                                      double * __restrict__ res);
 
-	double evaluateInnerSumX1_rl_preAllocated(unsigned int x_axis_points,
-	                                 float r,float l_x,float l_y,float l_z,
-					 float r_weight,float  l_weight,
-	                                 thrust::device_vector<double>& __restrict__ d_result, 
-	                                 double* __restrict__ d_sum, 
-	                                 int blocks, 
-	                                 int threads, 
-	                                 int gpu_num); 
-	    
+#include <vector>
 
-	double evaluateFourCenterIntegral( float* c,
-	                            int nr,  int nl,  int nx,
-	                            const std::string x1_type, double tol);
-	double evaluateFourCenterIntegral( float* c,
-                                  int nr,  int nl,  int nx,
-                                  const std::string x1_type,
-                                  int num_gpus);
+#include "cuslater.cuh"
+#include "grids.h"
+#include "utilities.h"
+// #include "evalInnerIntegral.h"
+namespace cuslater {
+__global__ void accumulateSum(double result, float r_weight, float l_weight,
+                              double* __restrict__ d_sum);
+__global__ void evaluateIntegrandReduceZ(int x_dim, float r, float l_x,
+                                         float l_y, float l_z,
+                                         double* __restrict__ res);
 
-}
+double evaluateInnerSumX1_rl_preAllocated(
+    unsigned int x_axis_points, float r, float l_x, float l_y, float l_z,
+    float r_weight, float l_weight,
+    thrust::device_vector<double>& __restrict__ d_result,
+    double* __restrict__ d_sum, int blocks, int threads, int gpu_num);
+
+double evaluateFourCenterIntegral(float* c, int nr, int nl, int nx,
+                                  const std::string x1_type, double tol);
+double evaluateFourCenterIntegral(float* c, int nr, int nl, int nx,
+                                  const std::string x1_type, int num_gpus);
+
+}  // namespace cuslater
