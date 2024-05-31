a3a04be6fce9c97179b7b86beda30d0eeb1340e4
MarkEwert03 <mewert03@gmail.com>
2024-05-30 17:34:51 -0700

    created gitignore and removed uneeded files and directories to prepare for merge to main

--- shell_scripts/simple_time_tests.sh
+++ shell_scripts/simple_time_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-> mylog.txt
-for ((a=6; a<=25; a++))
+> alphatimelog.txt
+for ((a=1; a<=25; a++))
 do
     output=$(./simple -a $a)
     last_line=$(echo "a = $a, $output" | tail -n 1)
     printf "a = $a\n"
     echo "$last_line"
-    echo "a = $a, $last_line" >> mylog.txt
+    echo "a = $a, $last_line" >> alphatimelog.txt
 done
 
