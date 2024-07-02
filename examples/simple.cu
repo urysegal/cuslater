785e937c755a70411611f6213dc8a09b22757123
MarkEwert03 <mewert03@gmail.com>
2024-07-02 16:25:32 -0700

    added more comprehensive command line parsing for alphas and tol

--- examples/simple.cu
+++ examples/simple.cu
@@ -16,29 +16,56 @@ int main(int argc, const char *argv[]) {
     int nz = 375;
     int nr = 89;
     int nl = 590;
+
+    // tolerance value for sum (if partial sum value < tol, then value is discarded)
     double tol = 1e-10;
 
     // α1 = alpha[0], α2 = alpha[1], α3 = alpha[2], α4 = alpha[3]
     float alpha[] = {1, 1, 1, 1};
-
-    if (argc == 3) {
-        std::string argv1 = std::string(argv[1]);
-        if (argv1 == "-a") {
-            float val = std::atoi(argv[2]);
-            alpha[0] = val;
-            alpha[1] = val;
-            alpha[2] = val;
-            alpha[3] = val;
-        } else if (argv1 == "-t") {
-            tol = std::stod(argv[2]);
-        }
-    }
 
     // c1 = (c1.x, c1.y, c1.z) = (c[0], c[1], c[2])
     // c2 = (c2.x, c2.y, c2.z) = (c[3], c[4], c[5])
     // c3 = (c3.x, c3.y, c3.z) = (c[6], c[7], c[8])
     // c4 = (c4.x, c4.y, c4.z) = (c[9], c[10], c[11])
     float c[] = {0, 0, 0, 1, 0, 0, 2, 0, 0, 3, 0, 0};
+
+    for (int i = 1; i < argc; ++i) {
+        if (std::strcmp(argv[i], "--help") == 0) {
+            // Print help message
+            std::cout << "Usage: ./simple [OPTION] [PARAMETERS ...]\n";
+            std::cout << "Options:\n";
+            std::cout << "  --help\t\tDisplay this help message and exit\n";
+            std::cout << "  -a a1 a2 a3 a4\tSet alpha values\n";
+            std::cout << "  -t tol\t\tSet tolerance value\n";
+            exit(EXIT_SUCCESS); // Exit after printing help message
+
+        } else if (std::strcmp(argv[i], "-a") == 0) {
+            // Read next 4 values as alphas
+            if (i + 4 >= argc) {
+                std::cerr << "Error: Fewer than 4 alpha parameters provided.\n";
+                exit(EXIT_FAILURE);
+            }
+            for (int j = 0; j < 4; ++j) {
+                // a_j = i+1, i+2, i+3, i+4
+                try {
+                    alpha[j] = std::atof(argv[j + 1 + i]);
+                } catch (...) {
+                    std::cerr << "Error: Insufficient values provided for -a option.\n";
+                    exit(EXIT_FAILURE);
+                }
+            }
+            i += 4; // Skip over processed alpha values
+
+        } else if (std::strcmp(argv[i], "-t") == 0) {
+            if (i + 1 >= argc) {
+                std::cerr << "Error: No tolerance parameter provided.\n";
+                exit(EXIT_FAILURE);
+            } else {
+                tol = std::atof(argv[i + 1]);
+                ++i; // Skip over tol value
+            }
+        }
+    }
 
     const std::string x1_type = "legendre"; // legendre or simpson
 
