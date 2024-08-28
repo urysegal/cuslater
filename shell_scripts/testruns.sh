251f9466bbb3c820b1cab4e3a32181e63835e09f
MarkEwert03 <mewert03@gmail.com>
2024-08-28 15:27:20 -0700

    generated output for x ranging from 25:25:500

--- shell_scripts/testruns.sh
+++ shell_scripts/testruns.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # List of parameter values to iterate over
-# xvalues=()
+xvalues=(25 50 75 100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500)
 # lvalues=()
 # rvalues=()
 # alphavalues=()
 
 # Log file where outputs will be saved
-logfile="output_CHANGENAME.log"
+logfile="tests/output_CHANGENAME.log"
 
 # Clear the logfile if it exists (optional)
 #> "$logfile"
 echo "" >> "$logfile"
 
 # Loop through values and execute the command
-
 
 for x in "${xvalues[@]}"; do
   echo "Running: ./simple -n $x"
