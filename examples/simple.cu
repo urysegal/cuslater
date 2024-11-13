4050d900216e0bf67891375aa32d23565c1a8b17
Gautam Luhana <gkluhana@gmail.com>
2024-11-13 15:35:08 -0800

    updt

--- examples/simple.cu
+++ examples/simple.cu
@@ -6,36 +6,142 @@
 #include <iomanip> // for std::setprecision
 #include <iostream>
 #include <chrono>
+#include <string>
 using namespace std;
 
 int
 main(int argc, const char *argv[])
 {
-        int nr = 89;
+        int nr = 97;
         int nl = 590;
-        int nx = 375;
+        int nx = 200;
+        int ny = 200;
+        int nz = 200;
         double tol = 1e-10;	
-	if ( argc == 2 and argv[1][0] == 'd' ) {
-		nr = 2;
-		nl = 6;
-		nx = 21;
-	} 
-	if ( argc == 3 and argv[1][0] == 't' ) {
-		tol = std::stod(argv[2]);	
-	} 
-        float c[] = {0,0,0,
+	float alpha[] = {1,1,1,1};
+
+        // c1 = (c1.x, c1.y, c1.z) = (c[0], c[1], c[2])
+        // c2 = (c2.x, c2.y, c2.z) = (c[3], c[4], c[5])
+    	// c3 = (c3.x, c3.y, c3.z) = (c[6], c[7], c[8])
+    	// c4 = (c4.x, c4.y, c4.z) = (c[9], c[10], c[11])
+	float c[] = {0,0,0,
                       1,0,0,
                       2,0,0,
                       3,0,0};
 
+	    for (int i = 1; i < argc; ++i) {
+        if (std::strcmp(argv[i], "--help") == 0) {
+            // Print help message
+            std::cout << "Usage: ./simple [OPTION] [PARAMETERS ...]\n";
+            std::cout << "Options:\n";
+            std::cout << "  --help\t\tDisplay this help message and exit\n";
+            std::cout << "  -a a1 a2 a3 a4\tSet alpha values\n";
+            std::cout << "  -c1 x1 y1 z1\t\tSets coordinates for c1 (c1.x, c1.y, c1.z)\n";
+            std::cout << "  -c2 x2 y2 z2\t\tSets coordinates for c2 (c2.x, c2.y, c2.z)\n";
+            std::cout << "  -c3 x3 y3 z3\t\tSets coordinates for c3 (c3.x, c3.y, c3.z)\n";
+            std::cout << "  -c4 x4 y4 z4\t\tSets coordinates for c4 (c4.x, c4.y, c4.z)\n";
+            std::cout << "  -t tol\t\tSet tolerance value\n";
+            std::cout << "  -l nl\t\tSet nl\n";
+            std::cout << "  -r nr\t\tSet nr\n";
+            std::cout << "  -n nx\t\tSet nx, ny, nz\n";
+            exit(EXIT_SUCCESS); // Exit after printing help message
+
+        } else if (std::strcmp(argv[i], "-a") == 0) {
+            // Read next 4 values as alphas
+            if (i + 4 >= argc) {
+                std::cerr << "Error: Fewer than 4 alpha parameters provided.\n";
+                exit(EXIT_FAILURE);
+            }
+            for (int j = 1; j <= 4; ++j) {
+                // a_j = i+1, i+2, i+3, i+4
+                try {
+                    alpha[j - 1] = std::atof(argv[i + j]);
+                } catch (...) {
+                    std::cerr << "Error: Insufficient numerical values provided for -a option.\n";
+                    exit(EXIT_FAILURE);
+                }
+            }
+            i += 4; // Skip over processed alpha values
+
+        } else if (argv[i][0] == '-' && argv[i][1] == 'c') {
+            int centNum = 0;
+            std::string argi = argv[i];
+            if (argi.length() != 3) {
+                std::cerr << "Error: Please provide 1 digit for the center number.\n";
+                exit(EXIT_FAILURE);
+            } else if (argi.substr(2, 1) != "1" && argi.substr(2, 1) != "2" &&
+                       argi.substr(2, 1) != "3" && argi.substr(2, 1) != "4") {
+                std::cerr << "Error: Please provide `-ci` where i = 1,2,3,4.\n";
+                exit(EXIT_FAILURE);
+            }
+            // shift by '0' to convert char->int
+            centNum = argi[2] - '0';
+            // Read next 3 values coordinates for ci
+            if (i + 3 >= argc) {
+                std::cerr << "Error: Fewer than 3 coordinates provided for c" << centNum << ".\n";
+                exit(EXIT_FAILURE);
+            }
+            for (int j = 0; j < 3; ++j) {
+                // ci_j = i+1, i+2, i+3
+                try {
+                    c[(centNum - 1) * 3 + j] = std::atof(argv[i + 1 + j]);
+                } catch (...) {
+                    std::cerr << "Error: Insufficient numerical values provided for -c" << centNum
+                              << " option.\n";
+                    exit(EXIT_FAILURE);
+                }
+            }
+            i += 3; // Skip over processed ci values
+
+        } else if (std::strcmp(argv[i], "-t") == 0) {
+            if (i + 1 >= argc) {
+                std::cerr << "Error: No tolerance parameter provided.\n";
+                exit(EXIT_FAILURE);
+            } else {
+                tol = std::atof(argv[i + 1]);
+                ++i; // Skip over tol value
+            }
+        } else if (std::strcmp(argv[i], "-l") == 0) {
+            if (i + 1 >= argc) {
+                std::cerr << "Error: No nl parameter provided.\n";
+                exit(EXIT_FAILURE);
+            } else {
+                nl = std::atof(argv[i + 1]);
+                ++i; // Skip over tol value
+            }
+        } else if (std::strcmp(argv[i], "-r") == 0) {
+            if (i + 1 >= argc) {
+                std::cerr << "Error: No nr parameter provided.\n";
+                exit(EXIT_FAILURE);
+            } else {
+                nr = std::atof(argv[i + 1]);
+                ++i; // Skip over tol value
+            }
+        } else if (std::strcmp(argv[i], "-n") == 0) {
+            if (i + 1 >= argc) {
+                std::cerr << "Error: No nx parameter provided.\n";
+                exit(EXIT_FAILURE);
+            } else {
+                nx = std::atof(argv[i + 1]);
+                ny = nx;
+                nz = nx;
+                ++i; // Skip over tol value
+            }
+        } else {
+            std::cerr << "Error: Invalid command line parameter. Use ./simple --help for more "
+                         "information.\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
         const std::string x1_type = "legendre"; //legendre or simpson
 
         auto start = std::chrono::high_resolution_clock::now();
-        auto sum = cuslater::evaluateFourCenterIntegral(c,nr,nl,nx,x1_type,tol);
+        auto sum = cuslater::evaluateFourCenterIntegral(c,alpha,nr,nl,nx,ny,nz,x1_type,tol);
         //auto sum = cuslater::evaluateFourCenterIntegral(c,nr,nl,nx,x1_type,4);
         auto end = std::chrono::high_resolution_clock::now();
 	    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        std::cout << "nr: " << nr << " nl: " << nl << " nx: " << nx << std::endl;
+        std::cout << "nr: " << nr << " nl: " << nl << " nx: " << nx << " ny: " << ny << " nz: " << nz << std::endl;
         std::cout << "result: " << std::fixed << std::setprecision(std::numeric_limits<double>::max_digits10) << sum << std::endl;
         std::cout << "Time Elapsed: " << duration.count()/1e6 << " seconds" << std::endl;
 }
--- src/evalIntegral.cu
+++ src/evalIntegral.cu
@@ -7,8 +7,13 @@ const double pi = 3.14159265358979323846;
 #include <thread>
 #define THREADS_PER_BLOCK 128 
 __constant__ float d_c[12];
+__constant__ float d_alpha[4];
 __constant__ float d_x_grid[600];
 __constant__ float d_x_weights[600];
+__constant__ float d_y_grid[600];
+__constant__ float d_y_weights[600];
+__constant__ float d_z_grid[600];
+__constant__ float d_z_weights[600];
 namespace cuslater{
 	__global__ void accumulateSum(double result, 
 						float r_weight, 
@@ -22,7 +27,7 @@ namespace cuslater{
 	}
 	
 	__global__
-	void evaluateIntegrandReduceZ(int x_dim,
+	void evaluateIntegrandReduceZ(int x_dim, int y_dim, int z_dim,
 	                                      float r, float l_x, float l_y, float l_z,
 	                                      double * __restrict__ res)
 	    {
@@ -61,10 +66,10 @@ namespace cuslater{
 	                     float zdiffc_2 = zvalue-d_c[5];
 	                   	 float zdiffc_3 = zvalue-d_c[8]+    r*l_z;
 	                   	 float zdiffc_4 = zvalue-d_c[11]+   r*l_z;
-	               	 	 float term1 = sqrt(xysq1 + zdiffc_1 * zdiffc_1);
-	                	 float term2 = sqrt(xysq2 + zdiffc_2 * zdiffc_2);
-	                   	 float term3 = sqrt(xysq3 + zdiffc_3*zdiffc_3 );
-	                   	 float term4 = sqrt(xysq4 + zdiffc_4*zdiffc_4);
+	               	 	 float term1 = d_alpha[0] * sqrt(xysq1 + zdiffc_1 * zdiffc_1);
+	                	 float term2 = d_alpha[1] * sqrt(xysq2 + zdiffc_2 * zdiffc_2);
+	                   	 float term3 = d_alpha[2] * sqrt(xysq3 + zdiffc_3*zdiffc_3 );
+	                   	 float term4 = d_alpha[3] * sqrt(xysq4 + zdiffc_4*zdiffc_4);
 	                   	 float exponent =  -term1 - term2- term3 -term4 + r ;
 	                   	 v += exp(exponent)*dxy*dz;
 	            	    }
@@ -72,11 +77,13 @@ namespace cuslater{
 		}
 	} //evaluateReduceInnerIntegrandz
 
-	double evaluateInnerSumX1_rl_preAllocated(unsigned int x_axis_points,
+	double evaluateInnerSum(unsigned int x_axis_points, unsigned int y_axis_points, unsigned int z_axis_points,
 	                                 float r, float l_x, float l_y, float l_z,
 					 float r_weight, float l_weight,
 	                                 thrust::device_vector<double>& __restrict__ d_result, 
-	                                 double* __restrict__ d_sum, 
+					 thrust::device_vector<double>& __restrict__ d_sorted,
+	                                 int i, int j, int nl,
+					 double* __restrict__ d_sum, 
 	                                 int blocks, 
 	                                 int threads, 
 	                                 int gpu_num) 
@@ -84,23 +91,21 @@ namespace cuslater{
          HANDLE_CUDA_ERROR(cudaSetDevice(gpu_num));
 	
          evaluateIntegrandReduceZ<<<blocks,threads>>>(
-	                               x_axis_points,r, l_x, l_y, l_z,
+	                               x_axis_points, y_axis_points, z_axis_points ,r, l_x, l_y, l_z,
 	                           raw_pointer_cast(d_result.data()));
         //for(long unsigned int i = 0; i < d_result.size(); i++)
 	    //std::cout << "d_result[" << i << "] = " << d_result[i] << std::endl;
         //Reduce vector on GPU within each block
+	    //thrust::sort(d_result.begin(), d_result.end());
 	    double delta_sum = thrust::reduce(d_result.begin(),d_result.end(),(double) 0.0, thrust::plus<double>());
-	    // Accumulate result on device
-		accumulateSum<<<1, 1>>>(delta_sum,
-	        			r_weight,
-	        			l_weight,
-	        			d_sum);
+	    
+	    d_sorted[ (i)*nl + (j) ] = delta_sum*r_weight*l_weight;
 	    return delta_sum;
 	}//evaluateInner
 
 
-    double evaluateFourCenterIntegral( float* c,
-                                int nr,  int nl,  int nx,
+    double evaluateFourCenterIntegral( float* c, float* alpha,
+                                int nr,  int nl,  int nx, int ny, int nz,
                                const std::string x1_type, double tol) {
 
         // read r grid
@@ -122,41 +127,80 @@ namespace cuslater{
                               l_weights);
         // Read x1 grid
         std::cout << "Reading x1 Grid Files" << std::endl;
-        const std::string x1_filepath = "grid_files/x1_"+x1_type+"_1d_" + std::to_string(nx) + ".grid";
+        const std::string x1_filepath = "grid_files_leg64/x1_"+x1_type+"_1d_" + std::to_string(nx) + ".grid";
         std::vector<float> x1_nodes;
         std::vector<float> x1_weights;
+	std::vector<float> y1_nodes;
+	std::vector<float> y1_weights;
+	std::vector<float> z1_nodes;
+	std::vector<float> z1_weights;
         float a;
         float b;
-        read_x1_1d_grid_from_file(x1_filepath, a, b, x1_nodes, x1_weights);
+	float ax = -10;
+	float bx = 11;
+	float ay = -10;
+	float by = 11;
+	float az= -10;
+	float bz=11;
+
+	generate_x1_from_std(ax,bx, x1_standard_nodes, x1_standard_weights, x1_nodes, x1_weights); 
+	generate_x1_from_std(ay,by, x1_standard_nodes, x1_standard_weights, y1_nodes, y1_weights); 
+	generate_x1_from_std(az,bz, x1_standard_nodes, x1_standard_weights, z1_nodes, z1_weights); 
+
+        //read_x1_1d_grid_from_file(x1_filepath, a, b, x1_nodes, x1_weights);
+        //read_x1_1d_grid_from_file(x1_filepath, a, b, y1_nodes, y1_weights);
+        //read_x1_1d_grid_from_file(x1_filepath, a, b, z1_nodes, z1_weights);
 
         std::cout << "Initializing Device Variables"<< std::endl;
         double* w = new double[3];
         unsigned int PX = x1_nodes.size();
+        unsigned int PY = y1_nodes.size();
+        unsigned int PZ = z1_nodes.size();
+
         int threads = THREADS_PER_BLOCK; // Max threads per block
-        int blocks = (PX*PX+threads-1)/threads; // Max blocks, better if multiple of SM = 80
+        int blocks = (PX*PY+threads-1)/threads; // Max blocks, better if multiple of SM = 80
 	    std::cout << "Total Threads: " << blocks*threads << std::endl;
-	    std::cout << "Total Grid Points: " << nx*nx*nx << std::endl; 
+	    std::cout << "Total Grid Points: " << nx*ny*nz << std::endl; 
 	    
-	    cudaMemcpyToSymbol(d_c, c, 12*sizeof(float));
+	cudaMemcpyToSymbol(d_c, c, 12*sizeof(float));
+	cudaMemcpyToSymbol(d_alpha, alpha, 4*sizeof(float));
         cudaMemcpyToSymbol(d_x_grid, x1_nodes.data(), PX*sizeof(float));
-	    cudaMemcpyToSymbol(d_x_weights, x1_weights.data(), PX*sizeof(float));
-	    double* d_sum;
+	cudaMemcpyToSymbol(d_x_weights, x1_weights.data(), PX*sizeof(float));
+        cudaMemcpyToSymbol(d_y_grid, y1_nodes.data(), PY*sizeof(float));
+	cudaMemcpyToSymbol(d_y_weights, y1_weights.data(), PY*sizeof(float));
+        cudaMemcpyToSymbol(d_z_grid, z1_nodes.data(), PZ*sizeof(float));
+	cudaMemcpyToSymbol(d_z_weights, z1_weights.data(), PZ*sizeof(float));
+
+	double* d_sum;
         thrust::device_vector<float> d_r_weights(nr);
         thrust::device_vector<float> d_l_weights(nl);
-	    thrust::device_vector<double> d_result(PX*PX);
+	thrust::device_vector<double> d_result(PX*PX);
+	thrust::device_vector<double> d_sorted(nr*nl,0);
 
         HANDLE_CUDA_ERROR(cudaMalloc(&d_sum, sizeof(double)));
-	    HANDLE_CUDA_ERROR(cudaMemset(d_sum, 0, sizeof(double)));
+	HANDLE_CUDA_ERROR(cudaMemset(d_sum, 0, sizeof(double)));
         double sum = 0.0;
         double delta_sum=0.0;
-	    int r_skipped = 0;
-        std::cout << "Evaluating Integral for all values of r and l" << std::endl;
+	int r_skipped = 0;
+	std::cout << "Evaluating Integral for all values of r and l with\n";
+        std::cout << "  a1=" << alpha[0] << ", a2=" << alpha[1] << ", a3=" << alpha[2]
+                  << ", a4=" << alpha[3] << "\n";
+        std::cout << "  c1 = (" << c[0] << ", " << c[1] << ", " << c[2] << ")\n";
+        std::cout << "  c2 = (" << c[3] << ", " << c[4] << ", " << c[5] << ")\n";
+        std::cout << "  c3 = (" << c[6] << ", " << c[7] << ", " << c[8] << ")\n";
+        std::cout << "  c4 = (" << c[9] << ", " << c[10] << ", " << c[11] << ")\n";
+        std::cout << "  Tolerance = " << tol << std::endl;
+	std::cout << " Legendre Grid Parameters: " << std::endl;
+	std::cout << " xgrid (ax , bx) : (" << ax << " , " << bx << ")" << std::endl;
+	std::cout << " ygrid (ay , by) : (" << ay << " , " << by << ")" << std::endl;
+	std::cout << " zgrid (az , bz) : (" << az << " , " << bz << ")" << std::endl;
+
                 for (int j = 0; j < nl; ++j) {
         		    for (int i=0; i < nr; ++i) {
-                        delta_sum = evaluateInnerSumX1_rl_preAllocated(nx,
+                        delta_sum = evaluateInnerSum(nx, ny, nz,
 						     r_nodes[i], l_nodes_x[j],l_nodes_y[j],l_nodes_z[j],
 						     r_weights[i], l_weights[j],
-                                                         d_result,
+                                                         d_result, d_sorted ,i,j,nl,
 						     d_sum, blocks, threads, 0);
 	            		if (delta_sum < tol )  {
 	            			r_skipped += nr - i;
@@ -167,8 +211,15 @@ namespace cuslater{
                     	std::cout << "computed for l_j:" << j <<"/" << nl << std::endl;
                     }
         }
-        HANDLE_CUDA_ERROR(cudaMemcpy(&sum, d_sum, sizeof(double), cudaMemcpyDeviceToHost));
-        sum *= (4.0/pi);
+        //HANDLE_CUDA_ERROR(cudaMemcpy(&sum, d_sum, sizeof(double), cudaMemcpyDeviceToHost));
+	thrust::sort(d_sorted.begin(), d_sorted.end());
+	//thrust:: host_vector<double> h_vector = d_sorted;
+	//for (long unsigned int i = 0; i < h_vector.size(); ++i) {
+        //	std::cout << h_vector[i] << " ";
+    	//}
+	sum  = thrust::reduce(d_sorted.begin(), d_sorted.end(), 0.0);
+	std::cout << "sum before multiplication " << sum << std::endl;
+        sum *= (4.0/pi) * std::pow(alpha[0] * alpha[1] * alpha[2] * alpha[3], 1.5);
 
         // sum up result, multiply with constant and return
         std::cout << "Tolerance: " << tol << std::endl;
